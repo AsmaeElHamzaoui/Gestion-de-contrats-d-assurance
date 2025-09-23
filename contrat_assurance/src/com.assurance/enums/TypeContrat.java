ed2c339fcd62887eb08cc7bc2ae9c2945d8fb3ce
Asmae el hamzaoui <asmaeelhamzaoui77@gamil.com>
2025-09-23 12:15:03 +0100

    la''implémentation des fichiers enums

---
+++ contrat_assurance/src/com.assurance/enums/TypeContrat.java
@@ -0,0 +1,7 @@
+package com.assurance.enums;
+
+public enum TypeContrat {
+    AUTOMOBILE,
+    MAISON,
+    MALADIE
+}
---
+++ contrat_assurance/src/com.assurance/enums/TypeSinistre.java
@@ -0,0 +1,7 @@
+package com.assurance.enums;
+
+public enum TypeSinistre {
+    ACCIDENT_VOITURE,
+    INCENDIE_MAISON,
+    MALADIE
+}
